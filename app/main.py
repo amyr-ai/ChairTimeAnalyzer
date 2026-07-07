5992db493bc6a607aebd11f62b2335e011165bac
Amyr <amr.azad@gmail.com>
2026-07-07 17:56:06 +0330

    Add folder browser and UI loader

--- app/main.py
+++ app/main.py
@@ -1,26 +1,17 @@
 import sys
 
-print("Program started")
-
 from PySide6.QtWidgets import QApplication
 
 from ui.main_window import MainWindow
 
 
 def main():
-    print("Creating QApplication")
 
     app = QApplication(sys.argv)
 
-    print("Creating window")
-
     window = MainWindow()
 
-    print("Showing window")
-
     window.show()
-
-    print("Entering event loop")
 
     sys.exit(app.exec())
 
--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,24 +1,32 @@
-from PySide6.QtWidgets import (
-    QMainWindow,
-    QLabel,
-    QWidget,
-    QVBoxLayout,
-)
+from PySide6.QtWidgets import QFileDialog
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
 
 
-class MainWindow(QMainWindow):
+class MainWindow:
+
     def __init__(self):
-        super().__init__()
 
-        self.setWindowTitle("Chair Time Analyzer")
-        self.resize(1100, 700)
+        ui_file = QFile("app/ui/main_window.ui")
+        ui_file.open(QFile.ReadOnly)
 
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        loader = QUiLoader()
 
-        layout = QVBoxLayout()
+        self.window = loader.load(ui_file)
 
-        title = QLabel("Chair Time Analyzer")
-        layout.addWidget(title)
+        ui_file.close()
 
-        central_widget.setLayout(layout)
+        self.window.btnBrowseFolder.clicked.connect(self.browse_folder)
+
+    def browse_folder(self):
+
+        folder = QFileDialog.getExistingDirectory(
+            self.window,
+            "Select Video Folder"
+        )
+
+        if folder:
+            self.window.txtVideoFolder.setText(folder)
+
+    def show(self):
+        self.window.show()
